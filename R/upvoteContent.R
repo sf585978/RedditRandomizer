83e8fd13b505fb75ba7501306d783d7a8eb28e92
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-07-16 11:03:01 -0400

    New Function
    Added function to upvote specified content from R.

---
+++ R/upvoteContent.R
@@ -0,0 +1,14 @@
+#' Upvote posts and comments
+#'
+#' @param id The fullname of the post (t3_) or comment (t1_) being upvoted
+#' @param dir The vote direction; one of (1, 0, -1)
+#' @return The status message from the POST function
+#' @export
+#' @example
+#' upvoteContent("t3_8wk7t8")
+
+upvoteContent <- function(id, dir) {
+  require(httr)
+  redditURL <- paste("https://oauth.reddit.com/api/vote/", id, sep = "")
+  POST(redditURL, config(token = token), user_agent("MY_USER_AGENT"))
+}
